6823eb1f8f1d8b74babba463fa9559a329d928df
extendedclip <fairviewent@yahoo.com>
2020-03-22 23:22:00 -0400

    Rename to MsgUtils and add a message method

--- src/main/java/me/clip/deluxetags/utils/MsgUtils.java
+++ src/main/java/me/clip/deluxetags/utils/MsgUtils.java
@@ -1,9 +1,14 @@
 package me.clip.deluxetags.utils;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 
-public class StringUtils {
+public class MsgUtils {
     public static String color(String input) {
         return ChatColor.translateAlternateColorCodes('&', input);
+    }
+
+    public static void msg(CommandSender s, String msg) {
+        s.sendMessage(color(msg));
     }
 }
